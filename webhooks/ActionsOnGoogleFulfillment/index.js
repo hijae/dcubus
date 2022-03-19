89cce72c3d554453e8321b56b33828eb050b9c90
Hijae song <hijae99@gmail.com>
2022-03-19 23:19:23 +0900

    주말 처리 추가

--- webhooks/ActionsOnGoogleFulfillment/index.js
+++ webhooks/ActionsOnGoogleFulfillment/index.js
@@ -6,163 +6,143 @@ const app = conversation();
 app.handle('gotime', conv => {
   const nowtime = new Date();
 
-	hour=(nowtime.getHours()+9)%24;
-  minute=nowtime.getMinutes();
-  if(hour<8)
-    {
-      text="08:10";
+  hour = (nowtime.getHours() + 9) % 24;
+  minute = nowtime.getMinutes();
+  weekend = nowtime.getDay();
+  if (weekend == 6 || weekend == 0) {
+    text = "weekend";
+  }
+  else {
+    if (hour < 8) {
+      text = "08:10";
     }
-  else if(hour==8)
-    {
-      if(minute<10)
-        {
-          text="08:10";
-        }
-      else if(minute<20)
-        {
-          text="08:20";
-        }
-      else if(minute<30)
-        {
-          text="08:30";
-        }
-      else if(minute<40)
-        {
-          text="08:40";
-        }
-      else if(minute<50)
-        {
-          text="08:50";
-        }
-      else
-        {
-          text="09:00";
-        }
+    else if (hour == 8) {
+      if (minute < 10) {
+        text = "08:10";
+      }
+      else if (minute < 20) {
+        text = "08:20";
+      }
+      else if (minute < 30) {
+        text = "08:30";
+      }
+      else if (minute < 40) {
+        text = "08:40";
+      }
+      else if (minute < 50) {
+        text = "08:50";
+      }
+      else {
+        text = "09:00";
+      }
     }
-  else if(hour==9)
-    {
-      if(minute<30)
-        {
-          text="09:30";
-        }
-      else if(minute<50)
-        {
-          text="09:50";
-        }
-      else
-        {
-          text="10:00";
-        }
+    else if (hour == 9) {
+      if (minute < 30) {
+        text = "09:30";
+      }
+      else if (minute < 50) {
+        text = "09:50";
+      }
+      else {
+        text = "10:00";
+      }
     }
-  else if(hour==10)
-    {
-      if(minute<30)
-        {
-          text="10:30";
-        }
+    else if (hour == 10) {
+      if (minute < 30) {
+        text = "10:30";
+      }
     }
-  else if(hour==11)
-    {
-      text="12:20";
+    else if (hour == 11) {
+      text = "12:20";
     }
-  else if(hour==12)
-    {
-      if(minute<20)
-        {
-          text="12:20";
-        }
-      else
-        {
-          text="stop";
-        }
+    else if (hour == 12) {
+      if (minute < 20) {
+        text = "12:20";
+      }
+      else {
+        text = "stop";
+      }
     }
-  else
-    {
-      text="stop";
+    else {
+      text = "stop";
     }
-  if(text=="stop")
-    {
-      text="오늘은 안심역 등교버스 운행이 종료되었습니다.";
-    }
-  else
-    {
-      text="다음 버스시간은 "+text+"입니다.";
-    }
-  conv.add(text+" 전체 안심역 등교 버스 시간표를 보려면 시간표라고 말씀해주세요.");
+  }
+  if (text == "stop") {
+    text = "오늘은 안심역 등교버스 운행이 종료되었습니다.";
+  }
+  if (text == "weekend") {
+    text = "주말에는 등교버스 운행을 하지 않습니다.";
+  }
+  else {
+    text = "다음 버스시간은 " + text + "입니다.";
+  }
+  conv.add(text + " 전체 안심역 등교 버스 시간표를 보려면 시간표라고 말씀해주세요.");
 });
 
 app.handle('backtime', conv => {
   const nowtime = new Date();
 
-	hour=(nowtime.getHours()+9)%24;
-  minute=nowtime.getMinutes();
-  if(hour<16)
-    {
-      text="오후 4:00";
+  hour = (nowtime.getHours() + 9) % 24;
+  minute = nowtime.getMinutes();
+  weekend = nowtime.getDay();
+  if (weekend == 6 || weekend == 0) {
+    text = "weekend";
+  }
+  else {
+  if (hour < 16) {
+    text = "오후 4:00";
+  }
+  else if (hour == 16) {
+    if (minute < 15) {
+      text = "오후 4:15";
     }
-  else if(hour==16)
-    {
-      if(minute<15)
-        {
-          text="오후 4:15";
-        }
-      else if(minute<30)
-        {
-          text="오후 4:30";
-        }
-      else if(minute<45)
-        {
-          text="오후 4:45";
-        }
-      else
-        {
-          text="오후 5:00";
-        }
+    else if (minute < 30) {
+      text = "오후 4:30";
     }
-  else if(hour==17)
-    {
-      if(minute<15)
-        {
-          text="오후 5:15";
-        }
-      else if(minute<30)
-        {
-          text="오후 5:30";
-        }
-      else
-        {
-          text="오후 7:00";
-        }
+    else if (minute < 45) {
+      text = "오후 4:45";
     }
-  else if(hour==18)
-    {
-      text="오후 7:00";
+    else {
+      text = "오후 5:00";
     }
-  else if(hour==19)
-    {
-      text="오후 09:00";
+  }
+  else if (hour == 17) {
+    if (minute < 15) {
+      text = "오후 5:15";
     }
-  else if(hour==20)
-    {
-      text="오후 09:00";
+    else if (minute < 30) {
+      text = "오후 5:30";
     }
-  else if(hour==21)
-    {
-      text="오후 10:00";
+    else {
+      text = "오후 7:00";
     }
-  else
-    {
-      text="stop";
-    }
-  if(text=="stop")
-    {
-      text="오늘 하교버스 운행이 종료되었습니다.";
-    }
-  else
-    {
-      text="다음 버스시간은 "+text+"입니다.";
-    }
-  conv.add(text+" 전체 안심역 하교 버스 시간표를 보려면 시간표라고 말씀해주세요.");
+  }
+  else if (hour == 18) {
+    text = "오후 7:00";
+  }
+  else if (hour == 19) {
+    text = "오후 09:00";
+  }
+  else if (hour == 20) {
+    text = "오후 09:00";
+  }
+  else if (hour == 21) {
+    text = "오후 10:00";
+  }
+  else {
+    text = "stop";
+  }
+  }
+  if (text == "stop") {
+    text = "오늘 하교버스 운행이 종료되었습니다.";
+  }
+  if (text == "weekend") {
+    text = "주말에는 하교버스 운행을 하지 않습니다.";
+  }
+  else {
+    text = "다음 버스시간은 " + text + "입니다.";
+  }
+  conv.add(text + " 전체 안심역 하교 버스 시간표를 보려면 시간표라고 말씀해주세요.");
 });
 
 exports.ActionsOnGoogleFulfillment = functions.https.onRequest(app);
